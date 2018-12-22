fb7e1b78d1aee0911191043f82423f0df1251739
Jieyou Xu <jieyouxu@outlook.com>
2018-12-22 16:10:10 +0800

    added terminals

---
+++ src/main/java/Terminals.java
@@ -0,0 +1,32 @@
+enum Terminals {
+    ZERO_DIGIT('0'),
+    ONE_DIGIT('1'),
+    TWO_DIGIT('2'),
+    THREE_DIGIT('3'),
+    FOUR_DIGIT('4'),
+    FIVE_DIGIT('5'),
+    SIX_DIGIT('6'),
+    SEVEN_DIGIT('7'),
+    EIGHT_DIGIT('8'),
+    NINE_DIGIT('9'),
+    PLUS_OPERATOR('+'),
+    MINUS_OPERATOR('-');
+
+    Terminals(char terminal) {
+        this.terminal = terminal;
+    }
+
+    boolean equals(char character) {
+        return terminal() == character;
+    }
+
+    char toChar() {
+        return terminal();
+    }
+
+    private final char terminal;
+
+    private char terminal() {
+        return terminal;
+    }
+}
